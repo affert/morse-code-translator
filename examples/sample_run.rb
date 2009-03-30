8cd5b70b9153cacb51c7b020f7908b8c6672f33c
Victor Goff <keeperotphones@gmail.com>
2009-03-30 22:41:05 +0800

    Separated run sample from library file
    Signed-off-by: Victor Goff <keeperotphones@gmail.com>

---
+++ examples/sample_run.rb
@@ -0,0 +1,30 @@
+require '../morse_code'
+STDOUT.sync = TRUE
+
+morse = MorseConverter.new
+
+puts 'Converting from morse to plain text:'
+puts morse.to_plain("-- --- .-. ... . / -.-. --- -.. . / - .... . / -. . .-- / -.. . .- -.. / .-.. .- -. --. ..- .- --. . .-.-.-")
+puts morse.to_plain("--- -- --.  .. ...  -- --- .-. ... .  -.-. --- -..")
+puts morse.to_plain("- .-- . . - ... / .- .-. . / . ...- . -. / ... .... --- .-. - . .-. / .-- .. - .... / -- --- .-. ... . / -.-. --- -..")
+puts morse.to_plain("-- --- .-. ... .   -.-. --- -.. .   .. ...   -... .- -..   ..-. --- .-.   - .-- . . - .. -. --")
+puts morse.to_plain(".. - /  .. ... /  .- /  .--. .-.. . .- ... ..- .-. . /  - --- /  -- . . - /  -.-- --- ..- /  .- ... /  .-- . .-.. .-.")
+puts
+
+# testing syntactic sugar
+puts 'Syntactic sugar for converting single words to morse:'
+puts morse.to_morse('RubyLearning')
+puts morse.rubylearning
+puts
+
+puts 'Converting to morse:'
+mc = morse.to_morse('HAHAHA Interesting! Thx PotHix.')
+puts mc
+puts morse.to_plain(mc)
+input = 'nil'
+until input.chomp == 'quit'
+  input = gets
+  puts input
+  puts morse.to_morse(input)
+end
+
