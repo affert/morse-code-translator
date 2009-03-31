6cd7b9841634afa9e144dc924b0d766e44bbcacf
Willian Molinari <willian.molinari@gmail.com>
2009-03-31 11:39:26 +0800

    Fixing a problem when i run doctest from other directory
    Signed-off-by: Victor Goff <keeperotphones@gmail.com>

--- test/doctest_morse_converter.rb
+++ test/doctest_morse_converter.rb
@@ -1,6 +1,6 @@
 =begin
 doctest: Setup
->> require '../morse_code'
+>> require File.dirname(__FILE__) + '/../morse_code'
 >> morse = MorseConverter.new
 doctest: Should return .- for letter a
 >> morse.to_morse 'a'
